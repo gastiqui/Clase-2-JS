f35e1f330131fabd88e9b454d1321e08d0744576
Gastón Quiroga <gasquiroga1@gmail.com>
2022-11-15 20:50:51 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const numero = prompt ( "INGRESE SU NUMERO" )
+
+if ( numero > 1000 ) {
+    console.log ( "Es mayor a 1000" )
+
+}
+
+const texto = prompt ( "INGRESE SU TEXTO" )
+
+if ( texto === "Hola" ) {
+    console.log ( "Es igual a Hola" )
+
+}
+
+const entre = prompt ( "INGRESE SU NUMERO" )
+
+if ( entre > 10 && entre < 50 ) {
+    console.log ( "Estas entre los numeros " )
+
+}
